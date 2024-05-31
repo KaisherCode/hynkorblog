d741b573f90267ae41313a51e618ee6418e6c6d4
Wilson Dev <hch.wilson@hynkor.com>
2024-05-31 10:41:36 -0500

    fix(greeting): type error

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -10,7 +10,7 @@ export default function Greeting({messages}) {
     <div className="mb-2">
       <h3 className="mb-2 text-2xl font-bold">{greeting}, Thank you for visiting!</h3>
       <button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-2 rounded-lg text-white font-semibold mb-1" onClick={() => setGreeting(randomMessage())}>
-        New greting
+        New greeting
       </button>
     </div>
   );
