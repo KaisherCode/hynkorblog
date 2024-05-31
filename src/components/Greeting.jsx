a936809b9ee81ad1abeedfc88e9e823a4d884d0d
Wilson Dev <hch.wilson@hynkor.com>
2024-05-31 10:27:12 -0500

    fix(greeting): texto en inglés

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,9 +8,9 @@ export default function Greeting({messages}) {
 
   return (
     <div className="mb-2">
-      <h3 className="mb-2 text-2xl font-bold">{greeting} ¡Gracias por tu visita!</h3>
+      <h3 className="mb-2 text-2xl font-bold">{greeting}, Thank you for visiting!</h3>
       <button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-2 rounded-lg text-white font-semibold mb-1" onClick={() => setGreeting(randomMessage())}>
-        Nuevo saludo
+        New greting
       </button>
     </div>
   );
